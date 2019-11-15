412be4560de0086ae4f1522dfb54dd68d78acc45
rae-glazier <glazier.home.life@gmail.com>
2019-11-15 10:49:15 -0600

    axios set up

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [char, setChar] = useState([]);
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    Axios.get('https://rickandmortyapi.com/api/character/')
+      .then( resp => {
+        console.log(resp);
+      })
+      .catch( err => {
+        console.log('tis ded jim', err)
+      })
+
   }, []);
 
   return (

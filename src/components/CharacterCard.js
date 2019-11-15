8c94713d1e245ae7fb99f0fd994be5051331816c
rae-glazier <glazier.home.life@gmail.com>
2019-11-15 11:40:42 -0600

    mapping complete

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard( props ) {
+  return (
+      <div className='char-card'>
+        <h2>{props.name}</h2>
+        <p>Species: {props.species}</p>
+        <p>Status: {props.status}</p>
+      </div>
+    )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,28 +1,42 @@
 import React, { useEffect, useState } from "react";
-import Axios from "axios";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
 
-  const [char, setChar] = useState([]);
+  const [chars, setChars] = useState([]);
+  
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-
-    Axios.get('https://rickandmortyapi.com/api/character/')
-      .then( resp => {
-        console.log(resp);
-      })
-      .catch( err => {
-        console.log('tis ded jim', err)
-      })
-
+    const getChars = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then( resp => {
+          console.log(resp.data.results);
+          setChars(resp.data.results)
+        })
+        .catch( err => {
+          console.log('tis ded jim', err)
+        })
+    }
+    
+    getChars();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>List of Characters</h2>
+      {chars.map(char => {
+
+      return (
+        <CharacterCard key={char.id} name={char.name} species={char.species} status={char.status}/>
+      )
+      })}
     </section>
   );
+
+
 }
